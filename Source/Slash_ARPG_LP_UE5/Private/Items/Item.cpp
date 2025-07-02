6593ea8fcc853f92b5891af24bcf7660ff4a7bd3
LittleVoid <08evangelion08@gmail.com>
2025-07-02 12:37:10 +0200

    [+] Added Basic Macros and Functions to ItemBP for testing/debugging

--- Source/Slash_ARPG_LP_UE5/Private/Items/Item.cpp
+++ Source/Slash_ARPG_LP_UE5/Private/Items/Item.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Items/Item.h"
+#include "Slash_ARPG_LP_UE5/DebugMacros.h"
+
+
 
 // Sets default values
 AItem::AItem()
@@ -9,19 +12,38 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 // Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+
 	
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+
+	RunningTime += DeltaTime;
+
+
 
 }
 
--- Source/Slash_ARPG_LP_UE5/Public/Items/Item.h
+++ Source/Slash_ARPG_LP_UE5/Public/Items/Item.h
@@ -12,15 +12,41 @@ class SLASH_ARPG_LP_UE5_API AItem : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AItem();
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere ,BlueprintReadWrite, Category ="Sin Parameters")
+	float Amplitude = 0.25f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sin Parameters")
+	float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	template<typename T>
+	T Avg(T First, T Second);
+
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+
+	float RunningTime;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
 
 };
+
+template<typename T>
+inline T AItem::Avg(T First, T Second)
+{
+	return T();
+}
