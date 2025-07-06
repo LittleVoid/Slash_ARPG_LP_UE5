9cc7948022af430d77f07bbd7de55f5d5d8ed672
LittleVoid <08evangelion08@gmail.com>
2025-07-06 18:59:29 +0200

    [+] Added Basic Movement(Debug) for now

--- Source/Slash_ARPG_LP_UE5/Public/Pawns/Bird.h
+++ Source/Slash_ARPG_LP_UE5/Public/Pawns/Bird.h
@@ -29,6 +29,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	UCapsuleComponent* Capsule;
