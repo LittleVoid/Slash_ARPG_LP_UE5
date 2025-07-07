3cf4e239c6b041987fbd68a2a932c1b2cca8413d
LittleVoid <08evangelion08@gmail.com>
2025-07-07 23:16:14 +0200

    [+] Added New Echo Char with Basic EnhancedInput Movement

---
+++ Source/Slash_ARPG_LP_UE5/Public/Characters/SlashCharacter.h
@@ -0,0 +1,57 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+
+#include "SlashCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UGroomComponent;
+class AItem;
+class ASoul;
+class ATreasure;
+class UAnimMontage;
+class USlashOverlay;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class SLASH_ARPG_LP_UE5_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+
+
+
+protected:
+	virtual void BeginPlay() override;
+
+	//Callbacks for Input
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* SlashCharacterMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MovementAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* LookAction;
+
+
+};
