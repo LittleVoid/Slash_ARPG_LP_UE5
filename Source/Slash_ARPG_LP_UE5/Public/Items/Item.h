54d6df369968bdbd6785a2ab8279fbd412529ddc
VoidCraft <08evangelion08@gmail.com>
2025-07-13 12:12:42 +0200

    [+] Added Weapon class, derived from Item class

--- Source/Slash_ARPG_LP_UE5/Public/Items/Item.h
+++ Source/Slash_ARPG_LP_UE5/Public/Items/Item.h
@@ -36,10 +36,10 @@ protected:
 	T Avg(T First, T Second);
 
 	UFUNCTION()
-	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION()
-	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 private:
 
