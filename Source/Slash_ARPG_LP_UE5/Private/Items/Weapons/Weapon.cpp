c66cf287f5d33d3f83ddde583b6768c81e0054bb
VoidCraft <08evangelion08@gmail.com>
2025-07-19 23:25:23 +0200

    [+] Added Breakable Actor

--- Source/Slash_ARPG_LP_UE5/Private/Items/Weapons/Weapon.cpp
+++ Source/Slash_ARPG_LP_UE5/Private/Items/Weapons/Weapon.cpp
@@ -99,5 +99,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 				HitInterface->GetHit(BoxHit.ImpactPoint);
 			}
 			IgnoreActors.AddUnique(BoxHit.GetActor());
+
+			CreateFields(BoxHit.ImpactPoint);
 	}
 }
