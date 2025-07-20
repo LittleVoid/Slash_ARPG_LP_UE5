02e2787364107427309b4cf8effa0315ea12ad93
VoidCraft <08evangelion08@gmail.com>
2025-07-20 15:37:51 +0200

    [+ Added Treasure BP/C++

---
+++ Source/Slash_ARPG_LP_UE5/Private/Items/Treasure.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Treasure.h"
+#include "Characters/SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
+
+
+
+ATreasure::ATreasure() : Super()
+{
+	AItem::Sphere->SetSphereRadius(125.f);
+}
+
+void ATreasure::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ASlashCharacter* SlashChacter = Cast<ASlashCharacter>(OtherActor);
+		if (SlashChacter)
+		{
+			if (PickUpSound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(this, PickUpSound, GetActorLocation());
+
+			}
+			Destroy();
+		}
+}
