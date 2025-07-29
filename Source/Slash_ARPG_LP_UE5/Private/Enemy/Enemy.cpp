435f264589212db51eedf5add1f213fd9bb6ae19
VoidCraft <08evangelion08@gmail.com>
2025-07-29 18:30:30 +0200

    [*] Refactoring Enemey.h/cpp

--- Source/Slash_ARPG_LP_UE5/Private/Enemy/Enemy.cpp
+++ Source/Slash_ARPG_LP_UE5/Private/Enemy/Enemy.cpp
@@ -36,6 +36,14 @@ AEnemy::AEnemy()
 	bUseControllerRotationRoll = false;
 }
 
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CheckCombatTarget();
+	CheckPatrolTarget();
+}
+
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
@@ -47,22 +55,16 @@ void AEnemy::BeginPlay()
 	}
 
 	EnemyController = Cast<AAIController>(GetController());
-
-		if (EnemyController && PatrolTarget)
-		{
-			FAIMoveRequest MoveRequest;
-			MoveRequest.SetGoalActor(PatrolTarget);
-			MoveRequest.SetAcceptanceRadius(15.f);
-			FNavPathSharedPtr NavPath;
-			EnemyController->MoveTo(MoveRequest, &NavPath);
-			TArray<FNavPathPoint>& PathPoints = NavPath->GetPathPoints();
-			for (auto& Point : PathPoints)
-			{
-				const FVector& Location = Point.Location;
-				DrawDebugSphere(GetWorld(), Location, 12.f, 12, FColor::Green, false, 10.f);
-			}
-		}
+	
+	MoveToTarget(PatrolTarget);
+	
 }
+
+void AEnemy::PatrolTimerFinished()
+{
+	MoveToTarget(PatrolTarget);
+}
+
 
 void AEnemy::Die()
 {
@@ -108,11 +110,42 @@ void AEnemy::Die()
 
 bool AEnemy::InTargetRange(AActor* Target, double Radius)
 {
+	if (Target == nullptr) return false;
 	const double DistanceToTarget = Target->GetDistanceTo(this);
 	DRAW_SPHERE_SingleFrame(GetActorLocation(), FColor::Red);
 	DRAW_SPHERE_SingleFrame(Target->GetActorLocation(), FColor::Green);
 
 	return DistanceToTarget <= Radius;
+}
+
+void AEnemy::MoveToTarget(AActor* Target)
+{
+	if (EnemyController == nullptr || Target == nullptr) return;
+
+	FAIMoveRequest MoveRequest;
+	MoveRequest.SetGoalActor(Target);
+	MoveRequest.SetAcceptanceRadius(15.f);
+	EnemyController->MoveTo(MoveRequest);
+}
+
+AActor* AEnemy::ChoosePatrolTarget()
+{
+	TArray<AActor*> ValidTargets;
+	for (AActor* Target : PatrolTargets)
+	{
+		if (Target != PatrolTarget)
+		{
+			ValidTargets.AddUnique(Target);
+		}
+	}
+	const int32 NumPatrolTargets = ValidTargets.Num();
+	if (NumPatrolTargets > 0)
+	{
+		const int32 TargetSelection = FMath::RandRange(0, NumPatrolTargets - 1);
+		return ValidTargets[TargetSelection];
+	}
+
+	return nullptr;
 }
 
 void AEnemy::PlayHitReactMontage(const FName SectionName)
@@ -125,54 +158,27 @@ void AEnemy::PlayHitReactMontage(const FName SectionName)
 	}
 }
 
-void AEnemy::Tick(float DeltaTime)
+
+void AEnemy::CheckPatrolTarget()
 {
-	Super::Tick(DeltaTime);
-
-	if (CombatTarget)
+	if (InTargetRange(PatrolTarget, PatrolRadius))
 	{
-		if (!InTargetRange(CombatTarget, CombatRadius))
-		{
-			CombatTarget = nullptr;
-			if (HealthBarWidget)
-			{
-				HealthBarWidget->SetVisibility(false);
-			}
-		}
-
+		PatrolTarget = ChoosePatrolTarget();
+		const float WaitTime = FMath::FRandRange(WaitMin, WaitMax);
+		GetWorldTimerManager().SetTimer(PatrolTimer, this, &AEnemy::PatrolTimerFinished, WaitTime);
 	}
+}
 
-	if (PatrolTarget && EnemyController)
+void AEnemy::CheckCombatTarget()
+{
+	if (!InTargetRange(CombatTarget, CombatRadius))
 	{
-		if (InTargetRange(PatrolTarget, PatrolRadius))
+		CombatTarget = nullptr;
+		if (HealthBarWidget)
 		{
-			TArray<AActor*> ValidTargets;
-			for (AActor* Target : PatrolTargets)
-			{
-				if (Target != PatrolTarget)
-				{
-					ValidTargets.AddUnique(Target);
-				}
-			}
-			const int32 NumPatrolTargets = ValidTargets.Num();
-			if (NumPatrolTargets > 0)
-			{
-				const int32 TargetSelection = FMath::RandRange(0, NumPatrolTargets - 1);
-
-
-				//AActor* Target = ValidTargets[TargetSelection];
-				//PatrolTarget = Target;
-				PatrolTarget = ValidTargets[TargetSelection];
-
-				FAIMoveRequest MoveRequest;
-				MoveRequest.SetAcceptanceRadius(15.f);
-				MoveRequest.SetGoalActor(PatrolTarget);
-				EnemyController->MoveTo(MoveRequest);
-				
-			}
+			HealthBarWidget->SetVisibility(false);
 		}
 	}
-
 }
 
 void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
--- Source/Slash_ARPG_LP_UE5/Public/Enemy/Enemy.h
+++ Source/Slash_ARPG_LP_UE5/Public/Enemy/Enemy.h
@@ -19,6 +19,8 @@ public:
 	AEnemy();
 
 	virtual void Tick(float DeltaTime) override;
+	void CheckPatrolTarget();
+	void CheckCombatTarget();
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
@@ -73,12 +75,25 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	double PatrolRadius = 200.f;
+
+	FTimerHandle PatrolTimer;
+	void PatrolTimerFinished();
+
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float WaitMin = 5.f;
+	
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+
+	float WaitMax = 10.f
+
 protected:
 	virtual void BeginPlay() override;
 
 	void Die();
 
 	bool InTargetRange(AActor* Target, double Radius);
+	void MoveToTarget(AActor* Target);
+	AActor* ChoosePatrolTarget();
 
 	// Play Montages
 
